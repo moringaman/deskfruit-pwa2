b3971d93d1a5844161ba376713d7a97db04d8e45
Leon Clarke <rob@webnostix.co.uk>
2023-10-23 11:05:01 +0000

    fix API_URL pt2

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -6,7 +6,7 @@ const API_URL = `${API_BASE_URL}/desks/`;
 console.log('API URL ', API_URL, env, process.env)
 
 const register = (deskId:string, email:string, password:string) => {
-  return axios.post(API_URL + "/signup", {
+  return axios.post(API_URL + "signup", {
     deskId,
     email,
     password,
@@ -16,7 +16,7 @@ const register = (deskId:string, email:string, password:string) => {
 
 const login = (deskId:string, password:string) => {
   return axios
-    .post(API_URL + "/signin", {
+    .post(API_URL + "signin", {
       deskId,
       password,
     }
