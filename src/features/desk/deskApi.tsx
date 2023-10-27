0774837ecba529286ffd58618931216946b9fd04
Leon Clarke <rob@webnostix.co.uk>
2023-10-27 15:32:13 +0000

    update api url fpr prod

--- src/features/desk/deskApi.tsx
+++ src/features/desk/deskApi.tsx
@@ -1,13 +1,13 @@
 // A mock function to mimic making an async request for data
 
-const BASE_API_URL = process.env.NODE_ENV === 'production' ? 'https://deskfruit-api.vercel.app' :
+const BASE_API_URL = process.env.NODE_ENV === 'production' ? 'https://api.deskfruit.co.uk' :
 'http://localhost:3001'
 
 export function fetchDesk(id:string) {
  return new Promise<any>((resolve,reject) => {
   try {
     console.log('fetching data...')
-    fetch(`${BASE_API_URL}'/desks/'${id}`)
+    fetch(`${BASE_API_URL}'/desks/'${id}`,)
         .then(response => {
           console.log('date ', response)
           resolve(response)
