665cd22a978529be7c5a55470a906d04c5dd8eba
Leon Clarke <rob@webnostix.co.uk>
2023-11-06 17:05:42 +0000

    update buttons

--- src/components/ui/StandardButton.tsx
+++ src/components/ui/StandardButton.tsx
@@ -8,7 +8,7 @@ export default function StandardButton(props:any) {
             <button onClick={action} 
               className={`w-[100px] ml-2 p-2 rounded-full flex-row ${ type === 'success' && 'bg-green'} bg-gray drop-shadow-lg flex items-center justify-center`}>
                     {loading && <Spinner /> } 
-                <p className="text-nickel text-xs text-black font-bold mx-2">{text}</p>
+                <p className="text-xs text-black font-bold mx-2">{text}</p>
                 {/* <Plus size={20} color="white"/> */}
             </button>
         </div>
